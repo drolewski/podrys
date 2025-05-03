a7c73b6f3641137cbe1e4df735c8b9ff9ead29bf
Dominik Rolewski <drolewski@gmail.com>
2025-05-03 15:29:39 +0200

    refactor lang construction

--- generate-sitemap.ts
+++ generate-sitemap.ts
@@ -1,23 +1,33 @@
-import { writeFileSync } from 'fs';
-import { resolve } from 'path';
+import {writeFileSync} from 'fs';
+import {resolve} from 'path';
 
 const baseUrl = 'https://drolewski.github.io/podrys';
 const date = (new Date()).toISOString();
 
-const routes = [
-    '',
-    'about',
-    'contact',
-];
+interface Link {
+    lang: string;
+    otherLangs: Map<string, string>;
+}
+
+const routesWithLang = new Map<string, Link>([
+    ['/', {lang: 'pl', otherLangs: new Map<string, string>([['en', '/en/']])}],
+    ['/en', {lang: 'en', otherLangs: new Map<string, string>([['pl', '/']])}],
+    ['/o-nas', {lang: 'pl', otherLangs: new Map<string, string>([['en', '/en/about']])}],
+    ['/en/about', {lang: 'en', otherLangs: new Map<string, string>([['pl', '/o-nas']])}],
+]);
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${routes
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+${[...routesWithLang]
     .map(
-        (route) => `  <url>
-    <loc>${baseUrl}/${route}</loc>
+        ([key, value]) => {
+            return `  <url>
+    <loc>${baseUrl}${key}</loc>
     <lastmod>${date}</lastmod>
-  </url>`
+    <xhtml:link rel="alternate" hreflang="${value.lang}" href="${baseUrl}${key}" />
+    ${[...value.otherLangs].map(([k, v]) => `<xhtml:link rel="alternate" hreflang="${k}" href="${baseUrl}${v}" />`)}
+  </url>`;
+        }
     )
     .join('\n')}
 </urlset>
