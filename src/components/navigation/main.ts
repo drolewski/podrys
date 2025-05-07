6518e1dc7f047203ca563b61267b0dbc977c0eaa
Dominik Rolewski <drolewski@gmail.com>
2025-05-07 18:27:11 +0200

    implement navbar

--- src/components/navigation/main.ts
+++ src/components/navigation/main.ts
@@ -1,2 +1,11 @@
 // @ts-ignore
 import "./navigation.scss"
+
+const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add('active');
+        console.log(link)
+    }
+});
